9f6b72712c0d45e2f258d1d5c9b6371a84f44b8e
Heebrah <ibrahimola08@gmail.com>
2025-07-22 15:28:36 +0100

    Add basic express app

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {"     res.send('Hello World!');   "});
+// Define a GET route for the root URL
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
+// Start the server
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
